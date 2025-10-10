3dc98d8b3791927c3adb404d02356043aa9419dc
Yokogamma <sopi@ya.ru>
2025-10-10 14:44:22 +0300

    add component language panel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
+import htmlInject from "vite-plugin-html-inject";
 
 export default defineConfig({
   base: "./", // чтобы пути были относительные
   build: {
     outDir: "dist", // папка сборки (по умолчанию dist)
   },
+  plugins: [htmlInject()],
 });
